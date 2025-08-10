d7b038822bc1f22a19871601f7c99c68af8ac76c
Higorhrs <higorrosa677@gmail.com>
2025-08-09 21:24:37 -0300

    Quinto commit

--- src/Entity/GerenciadorDeTarefas.java
+++ src/Entity/GerenciadorDeTarefas.java
@@ -1,5 +1,47 @@
 package Entity;
 
 public class GerenciadorDeTarefas {
+	
+	private int id;
+	private String nome;
+	private String descricao;
+	private String status;
+	
+	public GerenciadorDeTarefas(int id, String nome, String descricao, String status) {
+		this.id = id;
+		this.nome = nome;
+		this.descricao = descricao;
+		this.status = status;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	
+	
+	
+	
 
 }
